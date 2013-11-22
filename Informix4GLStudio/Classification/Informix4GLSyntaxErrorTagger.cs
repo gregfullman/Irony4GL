b37e7ef943d1bc27b3ffaf99a450843f0257fa93
gregfullman <greg.fullman@gmail.com>
2013-11-22 08:01:16 -0500

    Very primitive syntax error tagging works
    Will need to work on parser error messages to be more helpful. Also need
    the syntax error squigglies to show up in the right place.

--- Informix4GLStudio/Classification/Informix4GLSyntaxErrorTagger.cs
+++ Informix4GLStudio/Classification/Informix4GLSyntaxErrorTagger.cs
@@ -13,6 +13,18 @@ using System.Threading;
 
 namespace Informix4GLLanguage.Classification
 {
+    internal struct Informix4GLError
+    {
+        public Informix4GLError(Irony.Parsing.Token token, Irony.Parsing.ParserMessage message)
+        {
+            Token = token;
+            Message = message;
+        }
+
+        public Irony.Parsing.Token Token;
+        public Irony.Parsing.ParserMessage Message;
+    }
+
     [Export(typeof(ITaggerProvider))]
     [TagType(typeof(Informix4GLErrorTag))]
     [ContentType("Informix4GL")]
@@ -30,7 +42,7 @@ namespace Informix4GLLanguage.Classification
     {
         ITextBuffer buffer;
         ITextSnapshot snapshot;
-        List<Irony.Parsing.Token> errorTokens = new List<Irony.Parsing.Token>();
+        List<Informix4GLError> errorTokens = new List<Informix4GLError>();
         Timer delayTimer;
        
         public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
@@ -48,7 +60,7 @@ namespace Informix4GLLanguage.Classification
             if (spans.Count == 0 || this.errorTokens.Count == 0)
                 yield break;
 
-            List<Irony.Parsing.Token> currentErrors = this.errorTokens;
+            List<Informix4GLError> currentErrors = this.errorTokens;
             ITextSnapshot currentSnapshot = spans[0].Snapshot;
             SnapshotSpan entire = new SnapshotSpan(spans[0].Start, spans[spans.Count - 1].End).TranslateTo(currentSnapshot, SpanTrackingMode.EdgeExclusive);
             int startLineNumber = entire.Start.GetContainingLine().LineNumber;
@@ -56,17 +68,25 @@ namespace Informix4GLLanguage.Classification
             
             foreach (var error in currentErrors)
             {
-                if (error.Location.Line <= endLineNumber && error.Location.Line >= startLineNumber)
+                if (error.Token != null)
                 {
-                    int length = Math.Max(error.Length, 1);
-                    length = Math.Min(length, 100);
-                    length = Math.Min(currentSnapshot.Length - error.Location.Position - 1, length);
+                    if (error.Token.Location.Line <= endLineNumber && error.Token.Location.Line >= startLineNumber)
+                    {
+                        int length = Math.Max(error.Token.Length, 1);
+                        length = Math.Min(length, 100);
+                        length = Math.Min(currentSnapshot.Length - error.Token.Location.Position - 1, length);
+                        if (length < 0)
+                        {
+                            length = 0;
+                        }
 
-                    var line = currentSnapshot.GetLineFromLineNumber(error.Location.Line);
-                    var startPosition = line.Start.Position + error.Location.Column;
-                    yield return new TagSpan<Informix4GLErrorTag>(
-                        new SnapshotSpan(currentSnapshot, startPosition, length),
-                        new Informix4GLErrorTag(error.ValueString));
+                        var line = currentSnapshot.GetLineFromLineNumber(error.Token.Location.Line);
+                        var startPosition = line.Start.Position + error.Token.Location.Column;
+                        var errorMsg = error.Message != null ? error.Message.Message : error.Token.ValueString;
+                        yield return new TagSpan<Informix4GLErrorTag>(
+                            new SnapshotSpan(currentSnapshot, startPosition, length),
+                            new Informix4GLErrorTag(errorMsg));
+                    }
                 }
             }
         }
@@ -82,11 +102,40 @@ namespace Informix4GLLanguage.Classification
             var parser = Informix4GLFactory.Parser;
             var newErrors = new List<Irony.Parsing.Token>();
             var parseTree = parser.Parse(text);
+
+            // TODO: need a better way to do this...
+            // First want to look at the parser's error messages, and associate a token with them
+            foreach (var err in parseTree.ParserMessages)
+            {
+                if (parseTree.Tokens.Any(x => x.Location.Line == err.Location.Line))
+                {
+                    errorTokens.Add(new Informix4GLError(parseTree.Tokens.First(x => x.Location.Line == err.Location.Line), err));
+                }
+                else
+                {
+                    // look up one line (or more) until we find a token to assign this error to
+                    int i = err.Location.Line - 1;
+                    while (i >= 0)
+                    {
+                        if (parseTree.Tokens.Any(x => x.Location.Line == i))
+                        {
+                            errorTokens.Add(new Informix4GLError(parseTree.Tokens.First(x => x.Location.Line == i), err));
+                            break;
+                        }
+                    }
+                    if (i < 0)
+                    {
+                        errorTokens.Add(new Informix4GLError(null, err));
+                    }
+                }
+            }
+
+            // Get the tokens as well
             foreach (var token in parseTree.Tokens)
             {
                 if (token.IsError())
                 {
-                    errorTokens.Add(token);
+                    errorTokens.Add(new Informix4GLError(token, null));
                 }
             }
 
